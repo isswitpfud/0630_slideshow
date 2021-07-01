6b3041aa2db748eb7947462233796e0b072fe5a9
isswitpfud <isswitpfud@naver.com>
2021-07-01 15:40:39 +0900

    0701_슬라이드쇼가로형_구현완료

--- slide01_가로형/script/script.js
+++ slide01_가로형/script/script.js
@@ -1,6 +1,24 @@
 /* eslint-disable */
 
-$(function(){
+$(function () {
 
+    //fnSlide(); 함수의 내부코드가 실행=함수코드 (반복x)
+    setInterval(fnSlide, 3000); //일정시간마다 반복
 
+    function fnSlide() {
+        //슬라이드쇼 애니메이션 코드
+        $("div#shuttleFrame").animate({
+                "margin-left": "-780px"
+            },
+            3000,
+            //콜백함수(=익명함수)
+            function () {
+                $("#shuttleFrame>a:nth-child(1)").insertAfter("#shuttleFrame>a:nth-child(3)")
+
+                $("#shuttleFrame").css({
+                    "margin-left": "0"
+                });
+            }
+        ); //슬라이드쇼 애니메이션 코드
+    }
 });
